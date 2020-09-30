c547b3e6c97655b403390c7f6b88ad47f7b085fa
Arnstein Kleven <arnsteinkleven@gmail.com>
2020-09-30 13:00:09 +0200

    Add i3lock, solarized theme

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,6 @@
 USER=arnstein
 
-apt install i3 zsh wget git cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
+apt install i3 zsh wget git cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 dconf-cli
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
@@ -8,3 +8,13 @@ chsh -s /usr/bin/zsh $USER
 
 git clone https://github.com/jaagr/polybar.git
 cd polybar && ./build.sh
+
+
+git clone https://github.com/yvbbrjdr/i3lock-fancy-rapid
+cd i3lock-fancy-rapid
+make
+mv i3lock-fancy-rapid /usr/bin/
+
+git clone https://github.com/aruhier/gnome-terminal-colors-solarized.git
+cd gnome-terminal-colors-solarized
+./install.sh
