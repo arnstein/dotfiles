b215e5e416cafca56ed16e7bce3c4ae8c3c8dd0d
Arnstein Kleven <arnsteinkleven@gmail.com>
2020-10-02 14:13:21 +0200

    Add powerline fonts

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,6 @@
 USER=arnstein
 
-apt install i3 zsh wget git cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 dconf-cli libxkbcommon-x11-dev libxcbcommon-dev libxcb-xinerama0-dev libev-dev
+apt install i3 zsh wget git cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 dconf-cli libxkbcommon-x11-dev libxcbcommon-dev libxcb-xinerama0-dev libev-dev fonts-powerline
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
